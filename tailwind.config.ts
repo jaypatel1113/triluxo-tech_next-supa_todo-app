5b105cbae6fb7c2d2109a4078ef332a2f7db125c
Jay Patel <jay64441860@gmail.com>
2023-10-18 16:28:34 +0530

    added custom font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,11 +2,11 @@ import { type Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
-    content: ["./src/**/*.tsx"],
+    content: ['./src/**/*.tsx'],
     theme: {
         extend: {
             fontFamily: {
-                sans: ["var(--font-sans)", ...fontFamily.sans],
+                thunder: ['thunder', ...fontFamily.sans],
             },
         },
     },
