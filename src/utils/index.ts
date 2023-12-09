208e49aa052ee976cf2d9e3735a3717b1a1a1bbb
Jay Patel <jay64441860@gmail.com>
2023-12-09 18:26:27 +0530

    fix

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,8 +1,8 @@
 import { handleScroll } from "./scroll_to";
-import { getId } from "./getId";
+import { getId } from "./get_id";
 import { getAssets } from "./get_assets";
 import { loadAssets } from "./load_assets";
-import { getStaticProps } from "./getStaticProps";
+import { getStaticProps } from "./get_static_props";
 
 export {
     handleScroll,
